76cd17f015d978bd278c4ba0acdec0f97dd01bff
Tom <tomjuggler@gmail.com>
2021-12-28 15:36:32 +0200

    got an image

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 from PascalX import genescorer
 import os
 
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
 Gscorer = genescorer.chi2sum(window=50000,varcutoff=0.95)
 Gscorer.load_refpanel('/home/tom/PROGRAMMING/KGGRCh38/EUR.1KG.GRCh38',parallel=4)
 
@@ -19,19 +27,13 @@ R = Gscorer.score_chr(chrs=[2, 12])
 
 
 top_ten = Gscorer.get_topscores(N=10)
-print("top_ten: ", top_ten)
-
+print("top_ten: ", top_ten) #todo: None for now..
 
 #no luck with Manhattan Plot yet..
-m_plot = Gscorer.plot_Manhattan(R[0], labelList=['WDR12'])
-if m_plot:
-    m_plot = m_plot.save("/home/tom/Downloads/m_plot")
-    with open ("/home/tom/Downloads/m_plot", 'w') as f: 
-        m_plot = Gscorer.plot_Manhattan(R[0])
-        f.write("m_plot")
-    f.close()
+Gscorer.plot_Manhattan(R[0],
+labelList=['WDR12'])
 
-
+plt.savefig("m_plot.png")
 
 # print(R)
 print("done")
